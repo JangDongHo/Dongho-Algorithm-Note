0eaa0c6008237a5e0c850ccfc06f7428d16901e8
Replit user <>
2024-01-31 05:37:56 +0000

    볼링공 고르기

--- Greedy/Problem/만들 수 없는 금액.py
+++ Greedy/Problem/만들 수 없는 금액.py
@@ -9,7 +9,7 @@
 '''
 접근 방법
 - 동전을 화폐 단위 기준으로 정렬한 뒤에, 화폐 단위가 작은 동전부터 하나씩 확인하면서 target 변수를 업데이트하는 방법으로 최적의 해를 계산할 수 있다.
-![https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcRTvtg%2FbtrBuy1i3Sr%2FcZMexJEORqKmvyY7zUTryk%2Fimg.png](사진)
+![만들 수 없는 금액](https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcRTvtg%2FbtrBuy1i3Sr%2FcZMexJEORqKmvyY7zUTryk%2Fimg.png)
 '''
 
 n = int(input())
--- main.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-data = list(map(int, input.split()))
-data.sort()
-result = 0
-
-for i in range()
