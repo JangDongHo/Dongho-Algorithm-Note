97e147c0d271843059ac7624d863c54c5e45a4fc
Replit user <>
2024-02-18 05:12:12 +0000

    정수 삼각형

---
+++ DP/Problem/정수 삼각형.py
@@ -0,0 +1,39 @@
+'''
+난이도: 1.5
+목표 시간: 30분
+시간 제한: 2초
+메모리 제한: 128MB
+
+성공 여부: 실패
+'''
+'''
+접근 방식
+- array 변수는 초기 '정수 삼각형' 정보
+- dp 변수는 다이나믹 프로그래밍을 위한 2차원 테이블이라고 가정
+- 점화식: dp[i][j] = array[i][j] + max(dp[i-1][j-1], dp[i-1][j])
+- 단, dp 테이블에 접근해야 할 때마다 리스트의 범위를 벗어나지 않는지 체크할 필요가 있음
+- 또한, 구현의 편의상 초기 데이터를 담는 array 변수를 사용하지 않고, 바로 dp 테이블에 초기 데이터를 담아서 점화식에 따라서 dp 테이블을 갱신 할 수 있다.
+'''
+
+### 책 코드
+n = int(input())
+dp = []
+for _ in range(n):
+  dp.append(list(map(int, input().split())))
+
+for i in range(1, n):
+  for j in range(i + 1):
+    # 좌측 위로 이동
+    if j == 0:
+      up_left = 0
+    else:
+      up_left = dp[i - 1][j - 1]
+    # 우측 위로 이동
+    # 우측 위로 이동
+    if j == i:
+      up_right = 0
+    else:
+      up_right = dp[i - 1][j]
+    dp[i][j] = dp[i][j] + max(up_left, up_right)
+
+print(max(dp[-1]))
