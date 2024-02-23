28a4a63a1f51c9b63d04ab6bc405079def0bc497
Replit user <>
2024-02-23 09:03:31 +0000

    영역 구하기

--- DFS, BFS/BFS/영역 구하기.py
+++ DFS, BFS/BFS/영역 구하기.py
@@ -1,12 +1,64 @@
 '''
 플랫폼: 백준(https://www.acmicpc.net/problem/2583)
 난이도: 실버1
-풀이 시간: 30분
+목표 시간: 30분
 시간 제한: 1초
 메모리 제한: 128MB
 
-성공 여부: 실패
+1회독 성공 여부: 실패
+2회독 성공 여부: 성공
+풀이 시간: 22분
 '''
+
+### 2회독 성공 코드
+from collections import deque
+
+# 이동 방향 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+  q = deque()
+  q.append((x, y))
+  map_list[y][x] = 1
+  count = 1
+  while q:
+    [x, y] = q.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < N and 0 <= ny < M:
+        if map_list[ny][nx] == 0:
+          map_list[ny][nx] = 1
+          q.append((nx, ny))
+          count += 1
+  return count
+
+
+M, N, K = map(int, input().split())
+map_list = [[0] * N for _ in range(M)]
+result = []
+
+# 맵 생성
+for _ in range(K):
+  x1, y1, x2, y2 = map(int, input().split())
+  for i in range(y1, y2):
+    for j in range(x1, x2):
+      map_list[i][j] = 1
+
+# BFS 시작 지점 찾기
+for i in range(M):
+  for j in range(N):
+    if map_list[i][j] == 0:
+      count = bfs(j, i)
+      result.append(count)
+
+# 출력
+print(len(result))
+result.sort()
+for i in result:
+  print(i, end=' ')
 
 ### 해설 코드
 from collections import deque
--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 from collections import deque
 
+# 이동 방향 정의
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def bfs(x, y, miro_map):
+def bfs(x, y):
   q = deque()
   q.append((x, y))
+  map_list[y][x] = 1
+  count = 1
   while q:
-    x, y = q.popleft()
+    [x, y] = q.popleft()
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
-      if 0 <= nx < len(miro_map[0]) and 0 <= ny < len(miro_map):
-        if miro_map[ny][nx] == 1:
-          print(nx, ny)
+      if 0 <= nx < N and 0 <= ny < M:
+        if map_list[ny][nx] == 0:
+          map_list[ny][nx] = 1
           q.append((nx, ny))
-          miro_map[ny][nx] = miro_map[y][x] + 1
-  return miro_map[-1][-1]
+          count += 1
+  return count
 
 
-N, M = map(int, input().split())
-miro_map = []
+M, N, K = map(int, input().split())
+map_list = [[0] * N for _ in range(M)]
+result = []
 
-for _ in range(N):
-  miro_map.append(list(map(int, input())))
+# 맵 생성
+for _ in range(K):
+  x1, y1, x2, y2 = map(int, input().split())
+  for i in range(y1, y2):
+    for j in range(x1, x2):
+      map_list[i][j] = 1
 
-print(bfs(0, 0, miro_map))
+# BFS 시작 지점 찾기
+for i in range(M):
+  for j in range(N):
+    if map_list[i][j] == 0:
+      count = bfs(j, i)
+      result.append(count)
+
+# 출력
+print(len(result))
+result.sort()
+for i in result:
+  print(i, end=' ')
