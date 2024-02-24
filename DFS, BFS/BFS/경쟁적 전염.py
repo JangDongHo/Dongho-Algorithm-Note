2e1cccad591a337134bf9dbda82ead59d41a2e05
Replit user <>
2024-02-24 03:39:43 +0000

    경쟁적 전염

--- DFS, BFS/BFS/경쟁적 전염.py
+++ DFS, BFS/BFS/경쟁적 전염.py
@@ -4,7 +4,9 @@
 시간 제한: 1초
 메모리 제한: 256MB
 
-성공 여부: 실패
+1회독 성공 여부: 실패
+2회독 성공 여부: 성공
+풀이 시간: 34분
 '''
 '''
 접근 방법
@@ -13,6 +15,50 @@
 - 또한, 시간에 대한 개념도 체크해야 하므로 큐에 시간에 대한 정보도 같이 추가한다.
 - 큐에 바이러스에 대한 정보를 담을 때 마다 시간도 1초씩 늘린다.
 '''
+### 2회독 성공 코드
+from collections import deque
+
+N, K = map(int, input().split())
+map_list = [[0] * N for _ in range(N)]
+virus_list = []
+
+# 맵과 바이러스 리스트 생성
+for i in range(N):
+  input_list = list(map(int, input().split()))
+  for j in range(N):
+    if input_list[j] > 0:
+      virus = input_list[j]
+      map_list[i][j] = virus
+      virus_list.append((virus, 0, j, i))
+virus_list.sort()
+
+S, X, Y = map(int, input().split())
+
+# 방향 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+# BFS 생성
+def bfs(map_list):
+  q = deque(virus_list)
+  while q:
+    v, t, x, y = q.popleft()
+    if t >= S:
+      return map_list[X - 1][Y - 1]
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < N and 0 <= ny < N:
+        if map_list[ny][nx] == 0:
+          map_list[ny][nx] = v
+          q.append((v, t + 1, nx, ny))
+  return map_list[X - 1][Y - 1]
+
+
+print(bfs(map_list))
+
+### 책 코드
 
 from collections import deque
 
