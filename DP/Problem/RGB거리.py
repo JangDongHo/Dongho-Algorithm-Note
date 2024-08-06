ccf81edea70c579b942b344f2288e86d72ef7400
Jang-DongHo <donghoo18@naver.com>
2024-08-06 12:24:20 +0900

    RGB 거리

--- DP/Problem/RGB거리.py
+++ DP/Problem/RGB거리.py
@@ -1,3 +1,5 @@
+#https://www.acmicpc.net/problem/1149
+
 N = int(input())
 cost = [[0, 0, 0]]
 cost += [list(map(int, input().split())) for _ in range(N)]
@@ -14,6 +16,5 @@ for i in range(2, N + 1):
 	dp[i][0] = cost[i][0] + min(dp[i - 1][1], dp[i - 1][2])
 	dp[i][1] = cost[i][1] + min(dp[i - 1][0], dp[i - 1][2])
 	dp[i][2] = cost[i][2] + min(dp[i - 1][0], dp[i - 1][1])
-
 
 print(min(dp[N]))
