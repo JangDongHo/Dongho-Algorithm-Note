7a6073dcdda7c2e2882045d5c868ff77232f55d0
Jang-DongHo <donghoo18@naver.com>
2024-08-08 12:25:03 +0900

    LCS

--- DP/Problem/LCS.py
+++ DP/Problem/LCS.py
@@ -1,3 +1,28 @@
+# https://www.acmicpc.net/problem/9251
+
+### 풀이1. Bottom-Up 방식
+s1 = input()
+s2 = input()
+
+N, M = len(s1), len(s2)
+s1 = " " + s1
+s2 = " " + s2
+
+# 초기값 처리
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+
+# DP Table 갱신
+for n in range(1, N + 1):
+    for m in range(1, M + 1): # dp[n][m]
+        if s1[n] == s2[m]:
+            dp[n][m] = dp[n - 1][m - 1] + 1
+        else: 
+            dp[n][m] = max(dp[n - 1][m], dp[n][m - 1])
+
+print(dp[N][M])
+
+
+### 풀이2. Top-Down 방식
 import sys
 sys.setrecursionlimit(int(1e6))
 
