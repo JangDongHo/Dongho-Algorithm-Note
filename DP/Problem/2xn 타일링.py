869744350554b0f92bd85e35b980115d9397adee
Jang-DongHo <donghoo18@naver.com>
2024-08-15 12:16:08 +0900

    Update 2xn 타일링.py

--- DP/Problem/2xn 타일링.py
+++ DP/Problem/2xn 타일링.py
@@ -4,7 +4,7 @@
 
 n = int(input())
 
-dp = [0] * (n+1)
+dp = [0] * (n+2)
 
 dp[1] = 1
 dp[2] = 2
@@ -12,7 +12,7 @@ dp[2] = 2
 for i in range(3, n+1):
     dp[i] = (dp[i-1] + dp[i-2]) % 10007
 
-print(dp[-1])
+print(dp[n])
 
 ### 풀이2. Top-Down
 import sys
