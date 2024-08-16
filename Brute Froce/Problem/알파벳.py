9463e101f880d2c39851244c0be2c065dfe9501f
Jang-DongHo <donghoo18@naver.com>
2024-08-16 11:28:10 +0900

    알파벳

--- Brute Froce/Problem/알파벳.py
+++ Brute Froce/Problem/알파벳.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 # 이동 방향 정의
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -11,21 +9,17 @@ board = [list(input()) for _ in range(R)]
 # Solve
 answer = 1
 
-q = deque()
-q.append((0, 0, board[0][0]))
+st = set()
+st.add((0, 0, board[0][0]))
 
-while q:
-    x, y, used = q.popleft()
+while st:
+    x, y, used = st.pop()
 
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < C and 0 <= ny < R and board[ny][nx] not in used:
-            q.append((nx, ny, used + board[ny][nx]))
+            st.add((nx, ny, used + board[ny][nx]))
             answer = max(answer, len(used)+1)
 
 print(answer)
-
-
-
-
