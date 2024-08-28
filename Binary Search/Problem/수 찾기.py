4df4f0a6e3cd6448633b65e583c218a818e743e2
Jang-DongHo <donghoo18@naver.com>
2024-08-28 13:18:30 +0900

    Update 수 찾기.py

--- Binary Search/Problem/수 찾기.py
+++ Binary Search/Problem/수 찾기.py
@@ -1,16 +1,6 @@
 # https://www.acmicpc.net/problem/1920
 
 ### 풀이1. 이분 탐색
-N = int(input())
-arr = set(map(int, input().split()))
-
-M = int(input())
-nums = list(map(int, input().split()))
-
-for num in nums:
-	print(1 if num in arr else 0)
-
-### 풀이2. set 자료구조 이용
 def is_exist(arr, num):
 	left = 0
 	right = len(arr) - 1
@@ -38,3 +28,13 @@ nums = list(map(int, input().split()))
 
 for num in nums:
 	print(is_exist(arr, num))
+
+### 풀이2. set 자료구조 이용
+N = int(input())
+arr = set(map(int, input().split()))
+
+M = int(input())
+nums = list(map(int, input().split()))
+
+for num in nums:
+	print(1 if num in arr else 0)
