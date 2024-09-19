312f77dd854d092e071aa5558453ae166815ac43
Jang-DongHo <donghoo18@naver.com>
2024-09-19 14:34:50 +0900

    부분합

--- Binary Search/Problem/부분합.py
+++ Binary Search/Problem/부분합.py
@@ -1,3 +1,7 @@
+# https://www.acmicpc.net/problem/1806
+
+### 풀이 1. 이분 탐색 - O(NlogN)
+
 MAX_NUM = 100_001
 
 def parametric_search(left):
@@ -29,5 +33,30 @@ for left in range(1, N+1):
 	right = parametric_search(left)
 	if right <= N:
 		ans = min(ans, right - left + 1)
+
+print(ans if ans != MAX_NUM else 0)
+
+
+### 풀이 2. 투 포인터 - O(N)
+
+MAX_NUM = 100_001
+
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Solve (Tow Pointer)
+ans = MAX_NUM
+right = 0
+cur_sum = 0
+
+for left in range(N):
+	while (right < N) and (cur_sum < S):
+		cur_sum += arr[right]
+		right += 1
+
+	if (cur_sum >= S):
+		ans = min(ans, right - left)
+
+	cur_sum -= arr[left]
 
 print(ans if ans != MAX_NUM else 0)
