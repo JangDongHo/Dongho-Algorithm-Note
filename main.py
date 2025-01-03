34e69bb4900359e91955f9cc625187e618e1bac8
Jang-DongHo <donghoo18@naver.com>
2025-01-03 13:29:32 +0900

    [Fix] 곱셈

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-S = input()
-seen = set()
+def mod(a, b, m):
+    # Base Case
+    if b == 1:
+        return a % m
 
-for i in range(len(S)):
-	for j in range(i, len(S)):
-		seen.add(S[i:j + 1])
+    # Recursive Case
+    ret = mod(a, b//2, m)
+    ret = (ret * ret) % m
+    if b % 2 != 0:  # b가 홀수일 경우 추가적으로 a를 곱함
+        ret = (ret * a) % m
+    return ret
 
-print(len(seen))
+A, B, C = map(int, input().split())
+print(mod(A, B, C))
