96da7761f97a571ec75e06f971ec5ded85edc002
Jang-DongHo <donghoo18@naver.com>
2025-01-03 16:18:03 +0900

    Create BOJ 1629 곱셈.py

---
+++ Recursion/Problem/BOJ 1629 곱셈.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1629
+
+def mod(a, b, m):
+    # Base Case
+    if b == 1:
+        return a % m
+
+    # Recursive Case
+    ret = mod(a, b//2, m)
+    ret = (ret * ret) % m
+    if b % 2 != 0:  # b가 홀수일 경우 추가적으로 a를 곱함
+        ret = (ret * a) % m
+    return ret
+
+A, B, C = map(int, input().split())
+print(mod(A, B, C))
