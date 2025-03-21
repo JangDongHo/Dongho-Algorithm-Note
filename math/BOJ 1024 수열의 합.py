b29f2318e7541d73dc979232b97c5c063ccc525b
Jang-DongHo <donghoo18@naver.com>
2025-03-21 21:32:46 +0900

    BOJ 1024 수열의 합

--- math/BOJ 1024 수열의 합.py
+++ math/BOJ 1024 수열의 합.py
@@ -1,10 +1,12 @@
+# https://www.acmicpc.net/problem/1024
+
 N, L = map(int, input().split())
 
 for i in range(L, 101):
     x = N / i - (i + 1) / 2
-    if int(x) == x:
+    if int(x) == x: # x는 정수여야 한다.
         x = int(x)
-        if x + 1 >= 0:
+        if x + 1 >= 0: # 첫항(x+1)은 0보다 크거나 같아야한다.
             for j in range(x + 1, x + i + 1):
                 print(j, end=" ")
             break
