8a9dd6389b5c92ac8e20aa5e09b771877994e473
Jang-DongHo <donghoo18@naver.com>
2025-03-24 13:52:20 +0900

    BOJ 1058 친구

--- Graph Theory/Problem/BOJ 1058 친구.py
+++ Graph Theory/Problem/BOJ 1058 친구.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/1058
+
 # 입력 값 받기
 N = int(input())
 friends = [list(input()) for _ in range(N)]
@@ -9,7 +11,7 @@ for i in range(N):
         if friends[i][j] == 'Y': # i와 j가 직접 친구일 때
             two_friends.add(j)
             for k in range(N):
-                if friends[j][k] == 'Y': # 친구의 친구도 찾기
+                if friends[j][k] == 'Y' and k != i: # 친구의 친구도 찾기
                     two_friends.add(k)
     answer = max(answer, len(two_friends))
 
