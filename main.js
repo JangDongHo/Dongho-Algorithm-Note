8cb28d804c886538468ca10f263c07df2b73012e
Jang-DongHo <donghoo18@naver.com>
2025-04-16 23:25:29 +0900

    중간 저장

--- main.js
+++ main.js
@@ -1,33 +1,75 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  swap(idx1, idx2) {
+    [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+  }
+
+  heappush(value) {
+    this.heap.push(value);
+    this.bubbleUp();
+  }
+
+  heappop() {
+    if (this.size() <= 1) return this.heap.pop();
+
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+    return root;
+  }
+
+  bubbleUp() {
+    let idx = this.size() - 1;
+    while (idx > 0) {
+      const parentIdx = Math.floor((idx - 1) / 2);
+      if (this.heap[idx] >= this.heap[parentIdx]) break;
+
+      this.swap(idx, parentIdx);
+      idx = parentIdx;
+    }
+  }
+
+  bubbleDown() {
+    let idx = 0;
+    const length = this.size();
+    while (true) {
+      let leftIdx = 2 * idx + 1;
+      let rightIdx = 2 * idx + 2;
+      let smallest = idx;
+
+      if (leftIdx < length && this.heap[leftIdx] < this.heap[smallest]) {
+        smallest = leftIdx;
+      }
+      if (rightIdx < length && this.heap[rightIdx] < this.heap[smallest]) {
+        smallest = rightIdx;
+      }
+      if (smallest === idx) break;
+
+      this.swap(idx, smallest);
+      idx = smallest;
+    }
+  }
+}
+
 const fs = require('fs');
-
-function isPossible(length, cables, n) {
-	let sum = 0;
-	cables.forEach((e) => {
-		sum += Math.floor(e / length)
-	})
-	return sum >= n;
-}
-
-function binarySearch(left, right, cables, n) {
-	let answer = 0;
-	while(left <= right) {
-		const mid = Math.floor((left + right) / 2);
-
-		if (isPossible(mid, cables, n)) {
-			answer = mid;
-			left = mid + 1
-		} else {
-			right = mid - 1
-		}
-	}
-	return answer;
-}
-
 const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`;
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
+N = input[0]
+commands = input.slice(1).map(Number);
+hq = new MinHeap();
 
-const [K, N] = input[0].split(' ').map(Number);
-const cables = input.slice(1).map(Number);
-
-const answer = binarySearch(1, Math.max(...cables), cables, N)
-console.log(answer)
+for(let c of commands) {
+	if (c == 0) {
+		const min = hq.heappop();
+		console.log(min ? min : 0)
+	} else {
+		hq.heappush(c);
+	}
+}
