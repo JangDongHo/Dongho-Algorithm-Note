966d29341e07b6199b9fa7f2f98141d5e5bca27a
Jang-DongHo <donghoo18@naver.com>
2025-04-28 21:53:05 +0900

    BOJ 11399 ATM

--- Greedy/Problem/BOJ 11399 ATM.js
+++ Greedy/Problem/BOJ 11399 ATM.js
@@ -1,3 +1,5 @@
+// https://www.acmicpc.net/problem/11399
+
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`;
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
