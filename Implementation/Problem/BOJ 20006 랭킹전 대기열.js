56690c152f789026c15b272f1d46758c35967fa0
Jang-DongHo <donghoo18@naver.com>
2025-04-29 19:41:43 +0900

    BOJ 20006 랭킹전 대기열

--- Implementation/Problem/BOJ 20006 랭킹전 대기열.js
+++ Implementation/Problem/BOJ 20006 랭킹전 대기열.js
@@ -1,3 +1,5 @@
+// https://www.acmicpc.net/problem/20006
+
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`;
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
