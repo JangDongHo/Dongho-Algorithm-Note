f1fdba42c74e3135e18c26c4fac1fb6f81f4f65a
Dongho Jang <dongho18@gnu.ac.kr>
2025-06-09 21:40:03 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 604 ms, Memory: 41560 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,8 +1,6 @@
 from collections import deque
-
-# 0: 갈 수 없는 땅
-# 1: 갈 수 있는 땅
-# 2: 목표 지점
+import sys
+input = sys.stdin.readline
 
 # 입력 값 받기
 n, m = map(int, input().split())
@@ -13,15 +11,16 @@ for i in range(n):
     matrix.append(line)
 
 # BFS
-visited = [[-1] * m for _ in range(n)]
+dist = [[-1] * m for _ in range(n)]
+
 q = deque()
 for i in range(n):
     for j in range(m):
         if matrix[i][j] == 0:
-            visited[i][j] = 0
+            dist[i][j] = 0
         elif matrix[i][j] == 2:
             q.append((j, i))
-            visited[i][j] = 0
+            dist[i][j] = 0
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -31,10 +30,10 @@ while q:
     for d in range(4):
         nx = x + dx[d]
         ny = y + dy[d]
-        if (0 <= nx < m) and (0 <= ny < n) and (visited[ny][nx] == -1):
-            visited[ny][nx] = visited[y][x] + 1
+        if (0 <= nx < m) and (0 <= ny < n) and (dist[ny][nx] == -1):
+            dist[ny][nx] = dist[y][x] + 1
             q.append((nx, ny))
 
 # 출력
 for i in range(n):
-    print(" ".join(map(str, visited[i])))
+    print(" ".join(map(str, dist[i])))
