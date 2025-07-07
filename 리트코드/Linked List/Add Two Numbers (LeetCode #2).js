5b7b1b8ca85c49d06223086c869909e6acf1f535
Jang-DongHo <donghoo18@naver.com>
2025-07-07 14:05:22 +0900

    Add Two Numbers

---
+++ 리트코드/Linked List/Add Two Numbers (LeetCode #2).js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  const dummy = new ListNode();
+  let node = dummy;
+  let carry = 0;
+
+  while (l1 || l2 || carry) {
+    let sum = carry;
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    carry = Math.floor(sum / 10);
+    node.next = new ListNode(sum % 10);
+    node = node.next;
+  }
+
+  return dummy.next;
+};
