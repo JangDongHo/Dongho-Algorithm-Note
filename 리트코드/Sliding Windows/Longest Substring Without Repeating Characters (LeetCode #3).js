567fbab736b57c69fb7ca2ed84d876168305d1d6
Jang-DongHo <donghoo18@naver.com>
2025-07-09 13:57:10 +0900

    Longest Substring Without Repeating Characters

---
+++ 리트코드/Sliding Windows/Longest Substring Without Repeating Characters (LeetCode #3).js
@@ -0,0 +1,16 @@
+var lengthOfLongestSubstring = function (s) {
+  const set = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+};
