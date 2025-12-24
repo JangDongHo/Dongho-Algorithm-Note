4c1641cb4fc678d6f0d060cbc274b29274c91fcd
Dongho Jang <dongho18@gnu.ac.kr>
2025-12-24 15:05:52 +0900

    [level 2] Title: 완전범죄, Time: 0.93 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,32 +1,32 @@
 function solution(info, n, m) {
     // 1. 2차원 DP 생성
-    const dp = Array.from({ length: info.length + 1 }, () => Array(m + 1).fill(Infinity))
+    const dp = Array.from({ length: info.length + 1 }, () =>
+        Array(m).fill(Infinity)
+    );
     
     // 2. DP 초기화
-    dp[0][0] = 0
+    dp[0][0] = 0;
     
     // 3. DP 점화식
     for (let i = 1; i <= info.length; i++) {
-        const [aScore, bScore] = info[i - 1];
+        const [a_cost, b_cost] = info[i - 1];
         
-        for (let b = 0; b <= m; b++) {
-            if (dp[i - 1][b] + aScore < n) {
-                dp[i][b] = Math.min(dp[i][b], dp[i - 1][b] + aScore)   
+        for (let b = 0; b < m; b++) {
+            if (dp[i - 1][b] + a_cost < n) {
+                dp[i][b] = Math.min(dp[i][b], dp[i - 1][b] + a_cost)
             }
             
-            if (b + bScore < m) {
-                dp[i][b + bScore] = Math.min(dp[i][b], dp[i - 1][b])
+            if (b + b_cost < m) {
+                dp[i][b + b_cost] = Math.min(dp[i][b + b_cost], dp[i - 1][b]);
             }
         }
     }
-    
+
     // 4. 최종 답 구하기
     let answer = Infinity;
-    for (let b = 0; b <= m; b++) {
+    for (let b = 0; b < m; b++) {
         answer = Math.min(answer, dp[info.length][b]);
     }
-    
-    
     return answer === Infinity ? -1 : answer;
     
 }
