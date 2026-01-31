d8a08cf920648b8aba650029fd6a0f7df529dc56
Jang-DongHo <donghoo18@naver.com>
2026-01-31 17:33:45 +0900

    백준 10451 순열 사이클

--- main.js
+++ main.js
@@ -1,39 +1,31 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`;
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const input = fs.readFileSync(filePath).toString();
 
-const M = +input[0];
-const S = new Set();
-const output = [];
+const N = Number(input);
+const MOD = 10_007;
 
-for (let i = 1; i <= M; i++) {
-  const [command, val] = input[i].split(' ');
-  const num = +val;
-  
-  switch(command) {
-    case 'add':
-      S.add(num);
-      break;
-    case 'remove':
-      S.delete(num);
-      break;
-    case 'check':
-      output.push(S.has(num) ? 1 : 0);
-      break;
-    case 'toggle':
-      if (S.has(num)) S.delete(num);
-      else S.add(num);
-      break;
-    case 'all':
-      S.clear();
-      for (let j = 1; j <= 20; j++) {
-        S.add(j)
-      }
-      break;
-    case 'empty':
-      S.clear();
-      break;
+// dp[n][i] : n번째 자리에 i가 들어왔을 때 가능한 경우의 수
+const dp = Array.from({ length: N + 1 }, () => Array(10).fill(0));
+
+// DP 초기화
+dp[1].fill(1);
+
+// DP
+for (let n = 2; n <= N; n++) {
+  for (let i = 0; i <= 9; i++) {
+    let sum = 0;
+    for (let j = i; j <= 9; j++) {
+      sum = (sum + dp[n - 1][j]) % MOD;
+    }
+    dp[n][i] += sum;
   }
 }
 
-console.log(output.join('\n'));
+// 결과
+let answer = 0;
+for (let i = 0; i <= 9; i++) {
+  answer = (answer + dp[N][i]) % MOD;
+}
+
+console.log(dp[N]);
--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-N, r, c = map(int, input().split())
+from collections import deque
 
-def sol(N, r, c):
+T = int(input())
 
-    if N == 0:
-        return 0
-        
-    return 2 * (r % 2)+(c % 2) + 4 * sol(N - 1, r // 2, c // 2)
+def bfs(visited):
+    q = deque()
+    q.append(i)
+    visited[i] = True
 
-print(sol(N, r, c))
+    while(q):
+        node = q.popleft()
+
+        for adj_node in adj_list[node]:
+            if visited[adj_node]:
+                continue
+            q.append(adj_node)
+            visited[adj_node] = True
+
+for t in range(T):
+    N = int(input())
+
+    # 순열 생성
+    arr1 = list(range(1, N + 1))
+    arr2 = list(map(int, input().split()))
+
+    # 그래프 생성
+    adj_list = [[] for _ in range(N + 1)]
+    for i in range(N):
+        adj_list[arr1[i]].append(arr2[i])
+
+    # BFS 탐색
+    visited = [False] * (N + 1)
+    count = 0
+
+    for i in range(1, N + 1):
+        if not visited[i]:
+            bfs(visited)
+            count += 1
+
+    print(count)
+
