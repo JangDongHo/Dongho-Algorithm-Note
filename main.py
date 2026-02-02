cbf8c567d3706b69915ee5743751f6d71bdd8855
Jang-DongHo <donghoo18@naver.com>
2026-02-02 13:37:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,21 @@
-from collections import deque
-
 T = int(input())
 
-def bfs(visited):
-    q = deque()
-    q.append(i)
-    visited[i] = True
+def dfs(node):
+    next_node = node
+    while not visited[next_node]:
+        visited[next_node] = True
+        next_node = arr[next_node]
 
-    while(q):
-        node = q.popleft()
-
-        for adj_node in adj_list[node]:
-            if visited[adj_node]:
-                continue
-            q.append(adj_node)
-            visited[adj_node] = True
-
-for t in range(T):
+for _ in range(T):
     N = int(input())
+    arr = [0] + list(map(int, input().split()))
 
-    # 순열 생성
-    arr1 = list(range(1, N + 1))
-    arr2 = list(map(int, input().split()))
-
-    # 그래프 생성
-    adj_list = [[] for _ in range(N + 1)]
-    for i in range(N):
-        adj_list[arr1[i]].append(arr2[i])
-
-    # BFS 탐색
     visited = [False] * (N + 1)
     count = 0
 
     for i in range(1, N + 1):
         if not visited[i]:
-            bfs(visited)
+            dfs(i)
             count += 1
 
     print(count)
-
