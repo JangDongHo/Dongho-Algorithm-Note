922568ae9d7edb8f7b78682a31eb215dd220fe4f
Jang-DongHo <donghoo18@naver.com>
2026-05-10 15:44:58 +0900

    리트코드 LRU Cache

---
+++ linked list/리트코드. LRU Cache/solve.py
@@ -0,0 +1,23 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        value = self.cache.pop(key)
+        self.cache[key] = value
+        return value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+
+        elif len(self.cache) >= self.capacity:
+            oldest_key = next(iter(self.cache))
+            self.cache.pop(oldest_key)
+
+        self.cache[key] = value
